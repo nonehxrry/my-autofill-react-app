36cb64f3a5eed3b6dc5eb85736623ac862ce495d
Harjit Singh Bhadauriya <163171644+nonehxrry@users.noreply.github.com>
2025-06-13 21:35:21 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,10 +20,10 @@ function Footer() {
         <div className="logo">RezuWizard</div>
         <p className="tagline">Your gateway to standout resumes</p>
         <div className="social-icons">
-          <a href="#"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+          <a href="https://www.linkedin.com/company/rezuwizard/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
           <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
-          <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
-          <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+          <a href="https://x.com/RezuWizard"><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href="https://www.instagram.com/rezuwizard_official/"><FontAwesomeIcon icon={faInstagram} /></a>
         </div>
         <p className="copyright">© 2025 RezuWizard. All rights reserved.</p>
       </div>
